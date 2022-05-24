a8ea52de6306704a6f82dfed0fe41038648c8f0e
Shreya Achar <shrachar@student.pakuranga.school.nz>
2022-05-25 10:02:20 +1200

    list movies

--- FlaskUserDemo/app.py
+++ FlaskUserDemo/app.py
@@ -18,107 +18,103 @@ app.register_blueprint(setup)
 #        flash("You are not logged in.")
 #        return redirect('/login')
 
-#@app.route('/')
-#def home():
-#    return render_template("index.html")
+@app.route('/')
+def home():
+    return render_template("index.html")
 
-#@app.route('/login', methods=['GET', 'POST'])
-#def login():
-#    if request.method == 'POST':
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
 
-#        password = request.form['password']
-#        encrypted_password = hashlib.sha256(password.encode()).hexdigest()
+        password = request.form['password']
+        encrypted_password = hashlib.sha256(password.encode()).hexdigest()
 
-#        with create_connection() as connection:
-#            with connection.cursor() as cursor:
-#                sql = "SELECT * FROM users WHERE email = %s AND password = %s"
-#                values = (
-#                    request.form['email'],
-#                    encrypted_password
-#                )
-#                cursor.execute(sql, values)
-#                result = cursor.fetchone()
-#            if result:
-#                session['logged_in'] = True
-#                session['first_name'] = result['first_name']
-#                session['role'] = result['role']
-#                session['id'] = result['id']
-#                return redirect('/dashboard')
-#            else:
-#                flash("Invalid username or password.")
-#                return redirect('/login')
-#    else:
-#        return render_template("login.html")
+        with create_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM users WHERE email = %s AND password = %s"
+                values = (
+                    request.form['email'],
+                    encrypted_password
+                )
+                cursor.execute(sql, values)
+                result = cursor.fetchone()
+            if result:
+                session['logged_in'] = True
+                session['first_name'] = result['first_name']
+                session['role'] = result['role']
+                #session['id'] = result['id']
+                return redirect('/')
+            else:
+                flash("Invalid username or password.")
+                return redirect('/login')
+    else:
+        return render_template("login.html")
 
-#@app.route('/logout')
-#def logout():
-#    session.clear()
-#    return redirect('/')
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
 
-## TODO: Add a '/register' (add_user) route that uses INSERT
-#@app.route('/register', methods=['GET', 'POST'])
-#def add_user():
-#    if request.method == 'POST':
+# TODO: Add a '/register' (add_user) route that uses INSERT
+@app.route('/register', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
 
-#        password = request.form['password']
-#        encrypted_password = hashlib.sha256(password.encode()).hexdigest()
+        password = request.form['password']
+        encrypted_password = hashlib.sha256(password.encode()).hexdigest()
 
-#        if request.files['avatar'].filename:
-#            avatar_image = request.files["avatar"]
-#            ext = os.path.splitext(avatar_image.filename)[1]
-#            avatar_filename = str(uuid.uuid4())[:8] + ext
-#            avatar_image.save("static/images/" + avatar_filename)
-#        else:
-#            avatar_filename = None
+        if request.files['avatar'].filename:
+            avatar_image = request.files["avatar"]
+            ext = os.path.splitext(avatar_image.filename)[1]
+            avatar_filename = str(uuid.uuid4())[:8] + ext
+            avatar_image.save("static/images/" + avatar_filename)
+        else:
+            avatar_filename = None
 
-#        with create_connection() as connection:
-#            with connection.cursor() as cursor:
-#                sql = """INSERT INTO users
-#                    (first_name, last_name, email, password, avatar)
-#                    VALUES (%s, %s, %s, %s, %s)
-#                """
-#                values = (
-#                    request.form['first_name'],
-#                    request.form['last_name'],
-#                    request.form['email'],
-#                    encrypted_password,
-#                    avatar_filename
-#                )
-#                try:
-#                    cursor.execute(sql, values)
-#                    connection.commit()
-#                except pymysql.err.IntegrityError:
-#                    flash('Email has already been taken.')
-#                    return redirect('/register')
+        with create_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = """INSERT INTO users
+                    (first_name, last_name, email, password, avatar)
+                    VALUES (%s, %s, %s, %s, %s)
+                """
+                values = (
+                    request.form['first_name'],
+                    request.form['last_name'],
+                    request.form['email'],
+                    encrypted_password,
+                    avatar_filename
+                )
+                #try:
+                cursor.execute(sql, values)
+                connection.commit()
+                #except pymysql.err.IntegrityError:
+                #    flash('Email has already been taken.')
+                #    return redirect('/register')
 
+            #    sql = "SELECT * FROM users WHERE email = %s AND password = %s"
+            #    values = (
+            #        request.form['email'],
+            #        encrypted_password
+            #    )
+            #    cursor.execute(sql, values)
+            #    result = cursor.fetchone()
+            #if result:
+            #    session['logged_in'] = True
+            #    session['first_name'] = result['first_name']
+            #    session['role'] = result['role']
+            #    session['id'] = result['id']
+            #    return redirect('/')
 
-#                sql = "SELECT * FROM users WHERE email = %s AND password = %s"
-#                values = (
-#                    request.form['email'],
-#                    encrypted_password
-#                )
-#                cursor.execute(sql, values)
-#                result = cursor.fetchone()
-#            if result:
-#                session['logged_in'] = True
-#                session['first_name'] = result['first_name']
-#                session['role'] = result['role']
-#                session['id'] = result['id']
-#                return redirect('/')
-
-#    return render_template('users_add.html')
+    return render_template('users_add.html')
 
 # TODO: Add a '/dashboard' (list_users) route that uses SELECT
-@app.route('/dashboard')
+@app.route('/movies')
 def list_users():
-    #if session['role'] != 'admin':
-    #    return redirect('/')
-    #    flash("Only admin can access this page.")
     with create_connection() as connection:
         with connection.cursor() as cursor:
             cursor.execute("SELECT * FROM movies")
             result = cursor.fetchall()
-    return render_template('users_list.html', result=result)
+    return render_template('movies_list.html', result=result)
 
 ## TODO: Add a '/profile' (view_user) route that uses SELECT
 #@app.route('/view')
@@ -224,15 +220,15 @@ def list_users():
 #    else:
 #        return jsonify({ 'status': 'OK' })
 
-#if __name__ == '__main__':
-#    import os
+if __name__ == '__main__':
+    import os
 
-#    # This is required to allow flashing messages. We will cover this later.
-#    app.secret_key = os.urandom(32)
+    # This is required to allow flashing messages. We will cover this later.
+    app.secret_key = os.urandom(32)
 
-#    HOST = os.environ.get('SERVER_HOST', 'localhost')
-#    try:
-#        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-#    except ValueError:
-#        PORT = 5555
-#    app.run(HOST, PORT, debug=True)
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT, debug=True)
